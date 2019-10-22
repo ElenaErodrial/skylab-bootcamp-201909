f21419124a0af11d3e8381c0cbe05b99b820cc51
erodrial <elena.erodrial@gmail.com>
2019-10-22 11:05:35 +0200

    save changes

---
+++ staff/elena-rodriguez/duck-app-react/components/landing.jsx
@@ -0,0 +1,16 @@
+function Landing({onLogin, onRegister}) {
+
+    return <section className="view landing">
+        <h1 className="landing__title">Welcome to Duck App 🐥</h1>
+        <p className="landing__options">Please, proceed to <a href="" onClick={event => {
+            event.preventDefault()
+
+            onRegister()
+        }}>Register</a> or <a href="" onClick={event => {
+            event.preventDefault()
+
+            onLogin()
+
+        }}>Login</a>.</p>
+    </section>
+}
---
+++ staff/elena-rodriguez/duck-app-react/main.jsx
@@ -0,0 +1,19 @@
+function handleGoToLogin() {
+    debugger
+}
+
+function handleGoToRegister() {
+    debugger
+}
+
+function handleRegister(name, surname, email, password) {
+    debugger
+}
+
+// TODO login and search
+
+ReactDOM.render(<>
+    <Landing onLogin={handleGoToLogin} onRegister={handleGoToRegister}  />
+    <Register onRegister={handleRegister} />
+    <Login onLogin={handleLogin} />
+</>, document.getElementById('root'))
