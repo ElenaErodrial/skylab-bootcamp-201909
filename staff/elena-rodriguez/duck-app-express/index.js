a281be5a88da75d55c24f7abf12c0242c79b20fd
Elena Rodríguez <elenarodriguez@MacBook-Pro-de-Elena.local>
2019-11-06 20:25:18 +0100

    save changes

--- staff/elena-rodriguez/duck-app-express/index.js
+++ staff/elena-rodriguez/duck-app-express/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const Landing = require('./components/landing')
 const Login = require('./components/login')
 const Register = require('./components/register')
+const View = require('./components/view')
 
 const { argv: [, , port = 8080] } = process
 
@@ -10,24 +11,20 @@ const app = express()
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-    res.send(`<!DOCTYPE html>
-    <html lang="en">
-    
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Duck App</title>
-        <link rel="stylesheet" href="index.css">
-        <link rel="shortcut icon" href="icon.png" type="image/x-icon">
-    </head>
-    
-    <body>
-        ${Landing()}
-        ${Login()}
-        ${Register()}
-    </body>
-</html>`) 
+    res.send(View({body : Landing({register: '/register' , login: '/login'})})) 
 })
+
+app.get('/register', (req, res) => {
+    res.send(View({body : Register({landing: '/'})})) 
+})
+app.post('/register', (req, res) => {
+     
+})
+
+app.get('/login', (req, res) => {
+    res.send(View({body : Login({landing: '/'})})) 
+})
+
+
 
 app.listen(port, () => console.log(`server running on port ${port}`))
