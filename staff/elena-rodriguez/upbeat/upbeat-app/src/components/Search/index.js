90dc4388258b7ad913b9df7c3abda998c6903f1a
Elena Rodríguez <elenarodriguez@MacBook-Pro-de-Elena.local>
2019-12-05 12:52:51 +0100

    fix class to className on component search

--- staff/elena-rodriguez/upbeat/upbeat-app/src/components/Search/index.js
+++ staff/elena-rodriguez/upbeat/upbeat-app/src/components/Search/index.js
@@ -5,7 +5,7 @@ export default function ({ user }) {
     return <section className="search">
         <p className='greeting'>Hello, {user}! </p>
         <form className='searcher'>
-            <input type="text" class='searcher__bar' name="instruments" placeholder="what are you looking for?" />
+            <input type="text" className='searcher__bar' name="instruments" placeholder="what are you looking for?" />
             <button className="searcher__button">Search</button>
         </form>
     </section>
